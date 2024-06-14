dd266b4a0c542b2e584b7990d7b8a4d9bcc48b81
Antonio Martinez <pablo14pamg@gmail.com>
2024-06-14 11:00:50 -0500

    CrystalFetch class and methods added.

--- src/crystalfetch.cr
+++ src/crystalfetch.cr
@@ -1,6 +1,29 @@
-# TODO: Write documentation for `Crystalfetch`
-module Crystalfetch
-  VERSION = "0.1.0"
+require "system"
 
-  # TODO: Put your code here
+class CrystalFetch
+   def run()
+      get_os()
+      get_hostname()
+      get_cpu_count()
+   end
+   def get_os()
+      system("uname -a")
+   end
+   def get_hostname()
+      name = System.hostname
+      puts "#{name}"
+   end
+   def get_cpu_count()
+      cpu_num = System.cpu_count
+      puts "#{cpu_num}"
+   end
 end
+
+struct Information
+   property hostname;
+   property cpu_count;
+   property os_name;
+end
+
+fetch = CrystalFetch.new()
+fetch.run()
