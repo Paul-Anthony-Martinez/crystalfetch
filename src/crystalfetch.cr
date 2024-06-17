e272d2d9f60fbb1e3c9f78c9890b4aba6ae18045
Pablo Antonio <pablo14pamg@gmail.com>
2024-06-17 10:33:04 -0500

    Reorganized methods to fetch data and add the results into class properties.

--- src/crystalfetch.cr
+++ src/crystalfetch.cr
@@ -1,34 +1,48 @@
-require "system"
-
+#-----------------------------------#
+# Place true or false to enable ora #
+# disable the information that fetch#
+# will display!                     #
+#-----------------------------------#
+{
+   "os_name": true,
+   "hostname": true,
+   "cpu_count": true
+}
+#-----------------------------------#
 class CrystalFetch
+   @os_name = "";
+   @hostname = "";
+   @cpu_count = 0;
+   @kernel = {
+      name => "",
+      release => 0
+   }
    def run()
-      get_os()
-      get_hostname()
-      get_cpu_count()
+      get_os();
+      get_hostname();
+      get_cpu_count();
+      print_out();
    end
    def get_os()
-      system("uname -a")
+      cmd = "uname";
+      args = ["-s"];
+      stdout = IO::Memory.new();
+      stderr = IO::Memory.new();
+      status = Process.run(cmd, args: args, output: stdout);
+      @os_name = stdout.to_s();
    end
    def get_hostname()
-      name = System.hostname
-      puts "#{name}"
+      @hostname = System.hostname;
    end
    def get_cpu_count()
-      cpu_num = System.cpu_count
-      puts "#{cpu_num}"
+      @cpu_count = System.cpu_count.to_i32;
+   end
+   def print_out()
+      puts "os: #{@os_name}";
+      puts "hostname: #{@hostname}";
+      puts "cpu_count: #{@cpu_count}";
    end
 end
 
-struct Information
-   property hostname;
-   property cpu_count;
-   property os_name;
-end
-
-# main
-def main()
-  fetch = CrystalFetch.new()
-  fetch.run()
-end
-
-main();
+fetch = CrystalFetch.new();
+fetch.run();
