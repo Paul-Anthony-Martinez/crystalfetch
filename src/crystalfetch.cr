afe56d9f5c504d608c910f26716f4bfb83045fca
Pablo Antonio <pablo14pamg@gmail.com>
2024-06-19 11:49:42 -0500

    Add uname arguments array and if else statements to store kernel information accordingly.

--- src/crystalfetch.cr
+++ src/crystalfetch.cr
@@ -1,53 +1,72 @@
-#------------------------------------#
-# Place true or false to enable or   #
-# disable the information that fetch #
-# will display!                      #
-#------------------------------------#
-{
-   "os_name": true,
-   "hostname": true,
-   "cpu_count": true,
-   "kernel": {
-      "name": true,
-      "version": true,
-      "release": true
-   }
-}
-#-----------------------------------#
+require "json";
+
 class CrystalFetch
    @os_name = "";
    @hostname = "";
    @cpu_count = 0;
    @kernel = Kernel.new();
+   @rc_file = "";
+
    struct Kernel
       property version = ""
       property name = ""
       property release = ""
    end
+
+   def load_config()
+      file_path="res/.config/fetchrc.json"
+      begin
+         json=File.open(file_path) do |file|
+            @rc_file = JSON.parse(file);
+         end
+         except = Exception.new("File not found.")
+      raise except
+      rescue except
+         puts "#{STDERR}"
+         puts "#{except}"
+      end
+      puts @rc_file
+   end
+
    def run()
       get_os();
       get_hostname();
       get_cpu_count();
       get_kernel_info();
+      load_config
       print_out();
    end
+
    def get_os()
       cmd = "uname";
       args = ["-o"];
       @os_name = run_cmd(cmd, args);
    end
+
    def get_hostname()
       @hostname = System.hostname;
    end
+
    def get_cpu_count()
       @cpu_count = System.cpu_count.to_i32;
    end
+
    def get_kernel_info()
       cmd = "uname";
-      args = ["-s"];
-      @kernel.name = run_cmd(cmd, args);
+      args = ["-s", "-v", "-r"];
+      args.each do |arg|
+         cmd_arg = [arg]
+         cmd_out = run_cmd(cmd, cmd_arg);
+         if arg == "-s"
+            @kernel.name = cmd_out;
+         elsif arg == "-r"
+            @kernel.release = cmd_out;
+         elsif arg == "-v"
+            @kernel.version = cmd_out;
+         end
+      end
    end
-##########################################
+ 
    def run_cmd(cmd, args)
       stdout = IO::Memory.new();
       status = Process.run(
@@ -57,13 +76,14 @@ class CrystalFetch
       );
       return stdout.to_s();
    end
-##########################################
 
    def print_out()
       puts "os: #{@os_name}";
       puts "hostname: #{@hostname}";
       puts "cpu_count: #{@cpu_count}";
       puts "kernel_name: #{@kernel.name}";
+      puts "kernel_release: #{@kernel.release}";
+      puts "kernel_version: #{@kernel.version}";
    end
 end
 
